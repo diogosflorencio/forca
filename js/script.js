7aaf677b59f4942203c51f1e5e686d9b0a8124f3
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-02-23 23:29:25 -0300

    conclusão da lógica de adição das letras compativeis

--- js/script.js
+++ js/script.js
@@ -1,16 +1,66 @@
-let palavras = ["DAWDAWDWRdwdwdEQ", "ZZZZZ"]
+let palavras = [
+    "ENIGMA",
+    "AVENTURA",
+    "MISTERIO",
+    "ENCANTAMENTO",
+    "FANTASIA",
+    "DESAFIO",
+    "FANTASIA",
+    "FABULA",
+    "POESIA",
+    "HARRY-POTTER"
+];
+
+var dicas = [
+    "Pista: Desafio para a mente!",
+    "Pista: Muita adrenalina!",
+    "Pista: Sempre surpreendente!",
+    "Pista: Deixe-se levar pela imaginação!",
+    "Pista: O limite é a sua criatividade!",
+    "Pista: Muito movimento e emoção!",
+    "Pista: Onde os sonhos se tornam realidade!",
+    "Pista: Histórias incríveis esperam por você!",
+    "Pista: Transforme palavras em sentimentos!",
+    "Pista: A magia está no ar!"
+];
+
+let palavraDaVez = Math.round(Math.random()*10)
+let palavra = document.querySelector(".palavra");
+
+for(let i = 0; i < palavras[palavraDaVez].length; i++){
+    let traco = document.createElement("p");
+    traco.innerHTML = "-"
+    palavra.appendChild(traco);
+}
+
 
 const temLetra = (letra) => {
-    for (let i = 0; i < palavras[palavraDaVez()].length; i++) {
-        if(letra == palavras[palavraDaVez()][i]){
-            console.log(letra, i)
-        }else(console.log("fail"))
+    
+    for (let i = 0; i < palavras[palavraDaVez].length; i++) {
+
+        if(letra == palavras[palavraDaVez][i]){
+            escrevePalavra(letra)
+            return letra
+
+        }
         
+    }
+
+}
+
+function escrevePalavra(param) {
+    for(let i = 0; i < palavras[palavraDaVez].length; i++){
+        if (palavras[palavraDaVez][i] == param){ 
+            palavra.children[i].innerHTML = param
+            log(param)
+            
+        }
     }
 }
 
-function palavraDaVez(){
-    const posicao = Math.round(Math.random()*10);
-    console.log("pos" + posicao)
-    return posicao;
+
+
+// debug
+function log(param){
+    console.log(param)
 }
