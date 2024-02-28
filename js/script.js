522a147739a12351b4d585eb79ce739703573e7e
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-02-27 21:55:12 -0300

    add: criando boneco ao errar

--- js/script.js
+++ js/script.js
@@ -30,27 +30,34 @@ var dicas = [
 
 let palavraDaVez = Math.round(Math.random()*10)
 let palavra = document.querySelector(".palavra");
-
 let dica = document.querySelector(".dica");
 dica.innerHTML = dicas[palavraDaVez]
 
 for(let i = 0; i < palavras[palavraDaVez].length; i++){
     let traco = document.createElement("p");
-    traco.innerHTML = "-"
+    traco.innerHTML = "_"
     palavra.appendChild(traco);
 }
 
 
+let contadorLetras = 0;
+let contadorBoneco = 0;
 const temLetra = (letra) => {
-    
     for (let i = 0; i < palavras[palavraDaVez].length; i++) {
 
         if(letra == palavras[palavraDaVez][i]){
             escrevePalavra(letra)
             return letra
 
+        }else {
+            contadorLetras++;
         }
         
+    }
+    if (contadorLetras == palavras[palavraDaVez].length){
+        document.getElementsByClassName("boneco")[contadorBoneco].style="Display: block;"
+        contadorBoneco++;
+        contadorLetras = 0;
     }
 
 }
@@ -58,9 +65,7 @@ const temLetra = (letra) => {
 function escrevePalavra(param) {
     for(let i = 0; i < palavras[palavraDaVez].length; i++){
         if (palavras[palavraDaVez][i] == param){ 
-            palavra.children[i].innerHTML = param
-            log(param)
-            
+            palavra.children[i].innerHTML = param;
         }
     }
 }
